88fe28333c6e7f1656171fc9e43f36a06a808222
kiminzajnr <kiminzaeric@gmail.com>
2024-06-03 22:53:58 +0300

    POST state, city

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -11,3 +11,21 @@ states = [
 @app.get("/state")
 def get_states():
     return {"states": states}
+
+@app.post("/state")
+def create_state():
+    request_data = request.get_json()
+    new_state = {"name": request_data["name"], "cities": []}
+    states.append(new_state)
+
+    return new_state, 201
+
+@app.post("/state/<string:name>/city")
+def create_city(name):
+    request_data = request.get_json()
+    for state in states:
+        if state["name"] == name:
+            new_city = {"name": request_data["name"]}
+            state["cities"].append(new_city)
+            return new_city
+    return {"message": "State not found"}, 404
