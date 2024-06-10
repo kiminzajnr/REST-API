b3cdb78acfb249ec8b9cb52047d4c6796a1775b5
kiminzajnr <kiminzaeric@gmail.com>
2024-06-10 22:07:44 +0300

    refactor

--- resources/city.py
+++ resources/city.py
@@ -8,15 +8,37 @@ blp = Blueprint("Cities", __name__, description="Operations on items")
 
 @blp.route("/city/<string:item_id")
 class City(MethodView):
-    def get_city(self, city_id):
+    def get(self, city_id):
         try:
             return cities[city_id]
         except KeyError:
             return {"message": "City not found"}, 404
         
-    def delete_city(city_id):
+    def delete(city_id):
         try:
             del cities[city_id]
             return {"message": "City deleted."}
         except KeyError:
             return {"message": "City not found"}, 404
+        
+    def put(city_id):
+        city_data = request.get_json()
+        try:
+            city = cities[city_id]
+            city |= city_data
+            return city
+        except KeyError:
+            return {"message": "City not found."}, 404
+        
+@blp.route("/city")
+class CityList(MethodView):
+    def get(self):
+        return {"cities": list(cities.values())}
+    
+    def post(self):
+        city_data = request.get_json()
+        city_id = uuid.uuid4().hex
+        city = {**city_data, "id": city_id}
+        cities[city_id] = city
+
+        return city
