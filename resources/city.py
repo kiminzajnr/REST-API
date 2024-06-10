f81fc0b8d070020545c57ebd2c887ae00fcbdea7
kiminzajnr <kiminzaeric@gmail.com>
2024-06-10 23:00:48 +0300

    add: schmas

--- resources/city.py
+++ resources/city.py
@@ -2,6 +2,7 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from schemas import CityScema, CityUpdateSchema
 from db import cities
 
 blp = Blueprint("Cities", __name__, description="Operations on items")
@@ -20,9 +21,9 @@ class City(MethodView):
             return {"message": "City deleted."}
         except KeyError:
             return {"message": "City not found"}, 404
-        
-    def put(city_id):
-        city_data = request.get_json()
+    
+    @blp.arguments(CityUpdateSchema)
+    def put(self, city_data, city_id):
         try:
             city = cities[city_id]
             city |= city_data
@@ -35,8 +36,15 @@ class CityList(MethodView):
     def get(self):
         return {"cities": list(cities.values())}
     
-    def post(self):
-        city_data = request.get_json()
+    @blp.arguments(CityScema)
+    def post(self, city_data):
+        for city in cities.values():
+            if (
+                city_data["name"] == city["name"]
+                and city["state_id"] == city["state_id"]
+            ):
+                abort(404, message=f"City already exists.")
+
         city_id = uuid.uuid4().hex
         city = {**city_data, "id": city_id}
         cities[city_id] = city
