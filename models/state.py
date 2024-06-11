1d18c97e3ebe5e505c025f766e5aab6632718083
kiminzajnr <kiminzaeric@gmail.com>
2024-06-11 22:30:55 +0300

    create: simple sqlalchemy model

---
+++ models/state.py
@@ -0,0 +1,8 @@
+from db import db
+
+
+class StateModel(db.Model):
+    __tablename__ = "states"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
