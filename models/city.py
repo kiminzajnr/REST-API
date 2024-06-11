66bc2ef91bebe40754906a2a6dc0006d1ad9c6a5
kiminzajnr <kiminzaeric@gmail.com>
2024-06-11 22:46:42 +0300

    create: simple sqlalchemy model

--- models/city.py
+++ models/city.py
@@ -3,7 +3,11 @@ from db import db
 
 class CityModel(db.Model):
     __tablename__ = "cities"
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    state_id = db.Column(db.Integer, unique=False, nullable=False)
+
+    state_id = db.Column(
+        db.Integer, db.ForeignKey("states.id"), unique=False, nullable=False
+    )
+    state = db.relationship("StoreModel", back_populates="cities")
