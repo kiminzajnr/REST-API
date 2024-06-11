56caf789fff6d2e4c53a6201f3b4fc8e5baeb785
kiminzajnr <kiminzaeric@gmail.com>
2024-06-11 22:28:37 +0300

    create: simple sqlalchemy model

--- models/city.py
+++ models/city.py
@@ -2,6 +2,8 @@ from db import db
 
 
 class CityModel(db.Model):
+    __tablename__ = "cities"
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     state_id = db.Column(db.Integer, unique=False, nullable=False)
