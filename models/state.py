dd196418715a2cf886ff03a0b1d73e42a07cd66b
kiminzajnr <kiminzaeric@gmail.com>
2024-06-12 12:13:08 +0300

    add: delete related models

--- models/state.py
+++ models/state.py
@@ -7,4 +7,4 @@ class StateModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
 
-    cities = db.relationship("CityModel", back_populates="state", lazy="dynamic")
+    cities = db.relationship("CityModel", back_populates="state", lazy="dynamic", cascade="all, delete")
