8f16d753ec7ef21954cd3353f3db56f3b2271747
kiminzajnr <kiminzaeric@gmail.com>
2024-06-12 11:36:09 +0300

    add: flasksqlalchemy code to appfactory

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os 
+
 import models
 from db import db
 
@@ -7,7 +9,7 @@ from flask_smorest import Api
 from resources.city import blp as CityBlueprint
 from resources.state import blp as StateBlueprint
 
-def create_app():
+def create_app(db_url=None):
     app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -17,8 +19,14 @@ def create_app():
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
     api = Api(app)
+
+    with app.app_context():
+        db.create_all()
 
     api.register_blueprint(CityBlueprint)
     api.register_blueprint(StateBlueprint)
