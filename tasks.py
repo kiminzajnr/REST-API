faa3e7107078566686394dc3e46be0b0bb53284f
kiminzajnr <kiminzaeric@gmail.com>
2024-06-28 00:32:02 +0300

    feat: send html emails with mailgun

--- tasks.py
+++ tasks.py
@@ -1,13 +1,22 @@
 import os
 import requests
+
+import jinja2
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 domain = os.getenv("MAILGUN_DOMAIN")
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
 		f"https://api.mailgun.net/v3/{domain}/messages",
 		auth=("api", os.getenv("MAILGUN_API_KEY")),
@@ -16,6 +25,7 @@ def send_simple_message(to, subject, body):
 			"to": [to],
 			"subject": subject,
 			"text": body,
+            "html": html,
             },
         )
 
@@ -23,5 +33,6 @@ def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Successfully signed up",
-        f"Hi {username}! You have successfully signed up to the states REST API."
+        f"Hi {username}! You have successfully signed up to the states REST API.",
+        render_template("email/action.html", username=username)
     ),
